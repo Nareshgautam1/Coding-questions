1f56ac07e0fff6bafc24c7be08015a1760b22db1
Naresh <nareshgautam.ksp@gmail.com>
2021-10-03 20:36:04 +0530

    leetcode two_sum solution improve

--- sum_of-_ two.py
+++ sum_of-_ two.py
@@ -1,12 +1,11 @@
-A = [1,4,5,6,7,8]
-b = 14
-c= []
-i = 0
-j= i+1
-for i in range (len(A)):
-    for j in range(len(A)):
-         if A[i]+A[j] == b:
-             c.append(i)
-             c.append(j)
-
-print(c)
+nums =[3,2,4]
+target = 6
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        myArrayLen = len(nums)
+        i = 0
+        j = (i + 1)
+        for i in range(0, myArrayLen):
+            for j in range(i+1, myArrayLen):
+                if nums[j] == target - nums[i]:
+                    return [i,j]
